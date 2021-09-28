b94c86f3da4658da749410a5e8861538d7bb78f8
meridian34 <sobol.andrey.25@gmail.com>
2021-09-29 00:01:55 +0300

    added logic to remove anchor in a link

--- src/WebCrawler/Services/HtmlDocumentService.cs
+++ src/WebCrawler/Services/HtmlDocumentService.cs
@@ -7,20 +7,47 @@ namespace WebCrawler.Services
 {
     public class HtmlDocumentService : IHtmlDocumentService
     {
+        private readonly string _tag = "a";
+        private readonly string _anchor = "#";
+        private readonly string _hrefAttribute = "href";
+
         public IReadOnlyCollection<string> GetLinks(string htmlBody)
         {
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(htmlBody);
-            var links = GetLinkByTag(htmlDocument, "a").ToList();
+            var links = GetLinkByTag(htmlDocument, _tag).ToList();
             return links;
         }
 
-        private IEnumerable<string> GetLinkByTag(HtmlDocument document, string tag)
+        private IReadOnlyCollection<string> GetLinkByTag(HtmlDocument document, string tag)
         {
             var linkedPages = document.DocumentNode.Descendants(tag)
-                                              .Select(a => a.GetAttributeValue("href", null))
+                                              .Select(a => a.GetAttributeValue(_hrefAttribute, null))
                                               .Where(u => !string.IsNullOrEmpty(u)).ToList();
-            return linkedPages;
+            return PrepareLinks(linkedPages);
+        }
+
+        private IReadOnlyCollection<string> PrepareLinks(IReadOnlyCollection<string> links)
+        {
+            var linksList = new List<string>();
+            foreach (var link in links)
+            {
+                if (link.Contains(_anchor))
+                {
+                    linksList.Add(DeleteAnchor(link));
+                }
+                else
+                {
+                    linksList.Add(link);
+                }
+            }
+
+            return linksList;
+        }
+
+        private string DeleteAnchor(string link)
+        {
+            return link.Substring(0, link.LastIndexOf("#"));
         }
     }
 }
