adf940a5856d79e0941372fc5b7026a5235ea350
a.sobol <sobol.andrey.25@gmail.com>
2021-12-07 11:51:50 +0200

    refactoring HtmlDocumentService

--- src/WebCrawler/Services/HtmlDocumentService.cs
+++ src/WebCrawler/Services/HtmlDocumentService.cs
@@ -7,47 +7,19 @@ namespace WebCrawler.Services
 {
     public class HtmlDocumentService : IHtmlDocumentService
     {
-        private readonly string _tag = "a";
-        private readonly string _anchor = "#";
-        private readonly string _hrefAttribute = "href";
+        private const string _linkTag = "a";
+        private const string _anchor = "#";
+        private const string _hrefAttribute = "href";
 
         public IReadOnlyCollection<string> GetLinks(string htmlBody)
         {
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(htmlBody);
-            var links = GetLinkByTag(htmlDocument, _tag).ToList();
-            return links;
-        }
-
-        private IReadOnlyCollection<string> GetLinkByTag(HtmlDocument document, string tag)
-        {
-            var linkedPages = document.DocumentNode.Descendants(tag)
+            var links = htmlDocument.DocumentNode.Descendants(_linkTag)
                                               .Select(a => a.GetAttributeValue(_hrefAttribute, null))
-                                              .Where(u => !string.IsNullOrEmpty(u)).ToList();
-            return PrepareLinks(linkedPages);
-        }
+                                              .Where(u => !string.IsNullOrEmpty(u) && !u.Contains(_anchor)).ToList();
 
-        private IReadOnlyCollection<string> PrepareLinks(IReadOnlyCollection<string> links)
-        {
-            var linksList = new List<string>();
-            foreach (var link in links)
-            {
-                if (link.Contains(_anchor))
-                {
-                    linksList.Add(DeleteAnchor(link));
-                }
-                else
-                {
-                    linksList.Add(link);
-                }
-            }
-
-            return linksList;
-        }
-
-        private string DeleteAnchor(string link)
-        {
-            return link.Substring(0, link.LastIndexOf("#"));
+            return links;
         }
     }
 }
