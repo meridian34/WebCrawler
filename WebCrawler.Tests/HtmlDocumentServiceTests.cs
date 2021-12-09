7d18bca9bcbaffaa02f3e3eb58716c33f23e2f0c
a.sobol <sobol.andrey.25@gmail.com>
2021-12-09 15:11:32 +0200

    Added test project

---
+++ WebCrawler.Tests/HtmlDocumentServiceTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace WebCrawler.Tests
+{
+    public class HtmlDocumentServiceTests
+    {
+        
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
