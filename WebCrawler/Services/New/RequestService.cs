f2744495cb0991825669a80b0a1a3e2d60b2572c
a.sobol <sobol.andrey.25@gmail.com>
2021-12-10 14:35:32 +0200

    added new request service

---
+++ WebCrawler/Services/New/RequestService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+
+namespace WebCrawler.Services.New
+{
+    public class RequestService
+    {
+        private readonly IReadOnlyCollection<string> _contentTypesToHtml = new[] { "text/html; charset=utf-8", "text/html" };
+        private readonly IReadOnlyCollection<string> _contentTypesToXml = new[] { "text/xml", "application/xml", "text/xml; charset=UTF-8" };
+        private readonly IReadOnlyCollection<string> _targetContentTypes;
+       
+        public CrawlResult ScanUrlAsync(string url, out string page)
+        {
+            var timer = new Stopwatch();
+            var result = new CrawlResult();
+            result.Url = url;
+            page = null;
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                timer.Start();
+                var response = (HttpWebResponse)(request.GetResponse());
+                timer.Stop();
+                result.ElapsedMilliseconds = (int)timer.ElapsedMilliseconds;
+
+                if (_targetContentTypes.Contains(response.ContentType))
+                {
+                    using Stream receiveStream = response.GetResponseStream();
+                    using StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                    page = readStream.ReadToEnd();
+                }
+            }
+            catch (WebException)
+            {
+                timer.Stop();
+                result.ElapsedMilliseconds = (int)timer.ElapsedMilliseconds;
+            }
+
+            return result;
+        }
+    }
+    
+}
