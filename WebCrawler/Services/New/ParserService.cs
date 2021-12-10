c60bb848f8c9111e05724f30454ce5fb8a6276e0
a.sobol <sobol.andrey.25@gmail.com>
2021-12-10 16:44:04 +0200

    added new parse logic

--- WebCrawler/Services/New/ParserService.cs
+++ WebCrawler/Services/New/ParserService.cs
@@ -8,42 +8,79 @@ namespace WebCrawler.Services
 {
     public class ParserService
     {
-        public virtual IEnumerable<string> GetLinks(string data)
+        public virtual IEnumerable<string> GetSitemapLinks(string data)
         {
-            
             var linkList = new List<string>();
-            var tags = GetTags();
-            foreach(var tag in tags)
+            if(data == null)
             {
-                var position = 0;
-                while (position < data.Length)
-                {
-                    var startIndexTag = data.IndexOf(tag.Key, position);
-                    if(startIndexTag == -1)
-                    {
-                        break;
-                    }
-
-                    var endIndexTag = data.IndexOf(tag.Value, startIndexTag + tag.Key.Length);
-                    if(endIndexTag == -1)
-                    {
-                        break;
-                    }
-
-                    linkList.Add(data.Substring(startIndexTag + tag.Key.Length, endIndexTag  - (startIndexTag + tag.Key.Length)));
-                    position = endIndexTag + tag.Value.Length;
-                }
+                return linkList;
             }
+
+            var startTag = "<loc>";
+            var endTag = "</loc>";
+            var position = 0;
+
+            while (position < data.Length)
+            {
+                var startIndexTag = data.IndexOf(startTag, position);
+                if (startIndexTag == -1)
+                {
+                    break;
+                }
+
+                var endIndexTag = data.IndexOf(endTag, startIndexTag + startTag.Length);
+                if (endIndexTag == -1)
+                {
+                    break;
+                }
+
+                linkList.Add(data.Substring(startIndexTag + startTag.Length, endIndexTag - (startIndexTag + startTag.Length)));
+                position = endIndexTag + endTag.Length;
+            }
+
             return linkList;
         }
-
-        private Dictionary<string,string> GetTags()
+        public virtual IEnumerable<string> GetHtmlLinks(string html)
         {
-            return new Dictionary<string, string>
+            var linkList = new List<string>();
+
+            if (html == null)
             {
-                {@"href=""", @""""},
-                {"<loc>",  "</loc>"}
-            };
+                return linkList;
+            }
+
+            var startATag = "<a";
+            var endTag = ">";
+            var href = @"href=""";
+            var endHref = @"""";
+            var position = 0;
+
+            while (position < html.Length)
+            {
+                var startPositionTag = html.IndexOf(startATag, position);
+
+                if(startPositionTag == -1)
+                {
+                    break;
+                }
+
+                var endPositionTag = html.IndexOf(endTag, startPositionTag);
+                var tagBody = html.Substring(startPositionTag, endPositionTag - startPositionTag);
+                var positionHref = tagBody.IndexOf(href, 0);
+
+                if (positionHref > -1)
+                {
+                    var startLink = positionHref + href.Length;
+                    var endLink = tagBody.IndexOf(endHref, startLink);
+                    var link = tagBody.Substring(startLink, endLink - startLink);
+
+                    linkList.Add(link);
+                }
+
+                position = endPositionTag + endTag.Length;
+            }
+
+            return linkList;
         }
     }
 }
