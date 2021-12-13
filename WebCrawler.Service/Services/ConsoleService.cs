d07374fcbd8bc69904248e03e27f50550d747dc0
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 16:11:49 +0200

    added class wrapper over console

---
+++ WebCrawler.Service/Services/ConsoleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.ConsoleApplication.Services
+{
+    public class ConsoleService
+    {
+        public virtual string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public virtual void WriteLine(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
