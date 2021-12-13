0ed5e2b9805ec2e4e2d517af9167a9d2329fc9e9
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 15:38:43 +0200

    WebHandlerService has been replaced with a RequestService (WebHandlerService was delete)

---
+++ WebCrawler/Services/RequestService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using WebCrawler.Models;
+
+namespace WebCrawler.Services
+{
+    public class RequestService
+    {
+        private readonly IEnumerable<string> _targetContentTypes = new[] { "text/html; charset=utf-8", "text/html", "text/xml", "application/xml", "text/xml; charset=UTF-8" };
+       
+        public PerfomanceData ScanUrlAsync(string url, out string page)
+        {
+            var timer = new Stopwatch();
+            var result = new PerfomanceData();
+            result.Url = url;
+            page = null;
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                timer.Start();
+                var response = (HttpWebResponse)(request.GetResponse());
+                timer.Stop();
+                result.ElapsedMilliseconds = (int)timer.ElapsedMilliseconds;
+
+                using var receiveStream = response.GetResponseStream();
+                using var readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                page = readStream.ReadToEnd();
+            }
+            catch (WebException)
+            {
+                timer.Stop();
+                result.ElapsedMilliseconds = (int)timer.ElapsedMilliseconds;
+            }
+
+            return result;
+        }
+    }
+    
+}
