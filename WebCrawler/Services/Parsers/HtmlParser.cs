f77e4d34ad02329114c4ebc1c007b73e82b80304
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 15:35:52 +0200

    added new parsers for new crawlers

---
+++ WebCrawler/Services/Parsers/HtmlParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Services.Parsers
+{
+    public class HtmlParser
+    {
+        public virtual IEnumerable<string> GetHtmlLinks(string html)
+        {
+            var linkList = new List<string>();
+
+            if (html == null)
+            {
+                return linkList;
+            }
+
+            var startATag = "<a";
+            var endTag = ">";
+            var href = @"href=""";
+            var endHref = @"""";
+            var position = 0;
+
+            while (position < html.Length)
+            {
+                var startPositionTag = html.IndexOf(startATag, position);
+
+                if (startPositionTag == -1)
+                {
+                    break;
+                }
+
+                var endPositionTag = html.IndexOf(endTag, startPositionTag);
+                var tagBody = html.Substring(startPositionTag, endPositionTag - startPositionTag);
+                var positionHref = tagBody.IndexOf(href, 0);
+
+                if (positionHref > -1)
+                {
+                    var startLink = positionHref + href.Length;
+                    var endLink = tagBody.IndexOf(endHref, startLink);
+                    var link = tagBody.Substring(startLink, endLink - startLink);
+
+                    linkList.Add(link);
+                }
+
+                position = endPositionTag + endTag.Length;
+            }
+
+            return linkList;
+        }
+    }
+}
---
+++ WebCrawler/Services/Parsers/SitemapParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Services.Parsers
+{
+    public class SitemapParser
+    {
+        public virtual IEnumerable<string> GetSitemapLinks(string data)
+        {
+            var linkList = new List<string>();
+            if (data == null)
+            {
+                return linkList;
+            }
+
+            var startTag = "<loc>";
+            var endTag = "</loc>";
+            var position = 0;
+
+            while (position < data.Length)
+            {
+                var startIndexTag = data.IndexOf(startTag, position);
+                if (startIndexTag == -1)
+                {
+                    break;
+                }
+
+                var endIndexTag = data.IndexOf(endTag, startIndexTag + startTag.Length);
+                if (endIndexTag == -1)
+                {
+                    break;
+                }
+
+                linkList.Add(data.Substring(startIndexTag + startTag.Length, endIndexTag - (startIndexTag + startTag.Length)));
+                position = endIndexTag + endTag.Length;
+            }
+
+            return linkList;
+        }
+    }
+}
