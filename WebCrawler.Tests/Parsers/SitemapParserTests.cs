00df465e4d117fa0b2f25cb292ab3d2c26d491bc
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 18:33:26 +0200

    Added tests for sitemap parser

---
+++ WebCrawler.Tests/Parsers/SitemapParserTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Services.Parsers;
+using Xunit;
+
+namespace WebCrawler.Tests
+{
+    public class SitemapParserTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData(@"<loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/")]
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/</loc> <loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/latest-projects/", "https://www.ukad-group.com/")]
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/?param1=24</loc>", "https://www.ukad-group.com/latest-projects/?param1=24")]
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/#point1</loc>", "https://www.ukad-group.com/latest-projects/#point1")]
+        [InlineData(@"<loc>mailto:hi@ukad-group.com</loc>", "mailto:hi@ukad-group.com")]
+        public void GetSitemapLinks_SitemapXmlData_ShouldReturnLinksList(string html, params string[] expectedValue)
+        {
+            // arrange
+            var service = new SitemapParser();
+
+            //act
+            var result = service.GetSitemapLinks(html).ToArray();
+
+            //assert
+            Assert.Equal(result, expectedValue);
+        }
+    }
+}
