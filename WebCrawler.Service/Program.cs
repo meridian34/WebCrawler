c9177c95fb88407b1f229c5fd05739245da68062
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 16:14:45 +0200

    working logic has been removed from Program

--- WebCrawler.Service/Program.cs
+++ WebCrawler.Service/Program.cs
@@ -5,71 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebCrawler.Models;
 using WebCrawler.Services;
+using WebCrawler.Services.Crawlers;
+using WebCrawler.Services.Parsers;
+using WebCrawler.ConsoleApplication.Services;
 
-namespace WebCrawler.Service
+namespace WebCrawler.ConsoleApplication
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            var baseUrl = new Uri("https://www.ukad-group.com/123/");
-            var basePath = baseUrl.GetLeftPart(UriPartial.Authority);
-            var newUri = new Uri(basePath);
-
-
-            //var url = Console.ReadLine();
-
-            var webCrawler = new WebCrawlerService();
-            var result = await webCrawler.RunCrawler("https://www.ukad-group.com/");
-            PrintSitemapUniqueLink(result.Where(x=>x.IsSiteMap == true && x.IsSiteScan == false));
-            PrintSiteScanUniqueLink(result.Where(x => x.IsSiteMap == false && x.IsSiteScan == true));
-            PrintTimeResult(result);
-            PrintCount(result.Where(x => x.IsSiteScan == true).Count(), result.Where(x => x.IsSiteMap == true ).Count());
-           
-            Console.ReadKey();            
-        }
-
-        
-
-        private static void PrintSitemapUniqueLink(IEnumerable<CrawlResult> results)
-        {
-            PrintResult(results, "Urls FOUNDED IN SITEMAP.XML but not founded after crawling a web site");
-        }
-
-        private static void PrintSiteScanUniqueLink(IEnumerable<CrawlResult> results)
-        {
-            PrintResult(results, "Urls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml");
-        }
-
-        private static void PrintResult(IEnumerable<CrawlResult> results, string headerMessage)
-        {
-            Console.WriteLine($"{headerMessage}"); 
-            
-            foreach (var i in results)
-            {
-                Console.WriteLine($"{i.Url}");
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void PrintTimeResult(IEnumerable<CrawlResult> results)
-        {
-            Console.WriteLine($"Timing");
-
-            foreach (var i in results)
-            {
-                Console.WriteLine($"{i.Url}");
-                Console.WriteLine($"{i.ElapsedMilliseconds}");
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void PrintCount(int countScanResults, int countSitemapResults)
-        {
-            Console.WriteLine($"Urls(html documents) found after crawling a website: {countScanResults}");
-            Console.WriteLine($"Urls found in sitemap: {countSitemapResults}");
+            var htmlCrawler = new HtmlCrawler(new UrlValidatorService(), new RequestService(), new HtmlParser(), new LinkConvertorService());
+            var sitemapCrawler = new SitemapCrawler(new UrlValidatorService(), new RequestService(), new SitemapParser(), new LinkConvertorService());
+            var webCrawler = new WebCrawlerService(htmlCrawler, sitemapCrawler, new UrlValidatorService());
+            var consoleService = new ConsoleService();
+            var crawlerService = new CrawlerService(consoleService, webCrawler);
+            crawlerService.Run("https://www.ukad-group.com/");
         }
     }
 }
