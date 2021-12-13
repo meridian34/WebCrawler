9bd77ce1dfe52978f3281b5fba94d3660e270f9c
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 16:19:46 +0200

    unused using blocks and async has been removed

--- WebCrawler.Service/Program.cs
+++ WebCrawler.Service/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using WebCrawler.Models;
-using WebCrawler.Services;
+﻿using WebCrawler.Services;
 using WebCrawler.Services.Crawlers;
 using WebCrawler.Services.Parsers;
 using WebCrawler.ConsoleApplication.Services;
@@ -13,7 +7,7 @@ namespace WebCrawler.ConsoleApplication
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var htmlCrawler = new HtmlCrawler(new UrlValidatorService(), new RequestService(), new HtmlParser(), new LinkConvertorService());
             var sitemapCrawler = new SitemapCrawler(new UrlValidatorService(), new RequestService(), new SitemapParser(), new LinkConvertorService());
