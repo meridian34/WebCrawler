22f88828bf4a8767b25370227db28111b0a45ff7
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 15:35:17 +0200

    Added new html and sitemap crawlers

---
+++ WebCrawler/Services/Crawlers/HtmlCrawler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using WebCrawler.Models;
+using WebCrawler.Services.Parsers;
+
+namespace WebCrawler.Services.Crawlers
+{
+    public class HtmlCrawler
+    {
+        private readonly UrlValidatorService _validator;
+        private readonly RequestService _requestService;
+        private readonly HtmlParser _parserService;
+        private readonly LinkConvertorService _convertor;
+
+        public HtmlCrawler(
+            UrlValidatorService urlValidatorService,
+            RequestService requestService,
+            HtmlParser parserService,
+            LinkConvertorService linkConvertorService)
+        {
+            _validator = urlValidatorService;
+            _requestService = requestService;
+            _parserService = parserService;
+            _convertor = linkConvertorService;
+        }
+
+        public virtual IEnumerable<PerfomanceData> RunCrawler(string url)
+        {
+            var delay = 50;
+            var baseUrl = _convertor.GetRootUrl(url);
+            var linkQueue = new Queue<string>();
+            var resultList = new List<PerfomanceData>();
+            linkQueue.Enqueue(baseUrl);
+
+            while (linkQueue.Count > 0)
+            {
+                var link = linkQueue.Dequeue();
+                var resultsContainsLink = resultList.Any(x => x.Url == link);
+                var notValidLink = !_validator.IsValidLink(link);
+                var linkIsNotCorrect = !_validator.IsCorrectLink(link)
+                    || !_validator.ContainsBaseUrl(link, baseUrl);
+
+                if (notValidLink || resultsContainsLink || linkIsNotCorrect)
+                {
+                    continue;
+                }
+
+                Thread.Sleep(delay);
+                var result = _requestService.ScanUrlAsync(link, out string html);                
+                resultList.Add(result);
+                var parsedLinks = _parserService.GetHtmlLinks(html);
+
+                foreach (var newLink in parsedLinks)
+                {
+
+
+                    var correctLink = _validator.ContainsBaseUrl(newLink, baseUrl)
+                         ? newLink
+                         : _convertor.ConvertRelativeToAbsolute(newLink, baseUrl);
+
+                    var notContainsLink = !linkQueue.Any(x => x == correctLink);
+
+                    if (notContainsLink)
+                    {
+                        
+                        linkQueue.Enqueue(correctLink);
+                    }
+                }
+            }
+            
+            return resultList;
+        }
+    }
+}
---
+++ WebCrawler/Services/Crawlers/SitemapCrawler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using WebCrawler.Models;
+using WebCrawler.Services.Parsers;
+
+namespace WebCrawler.Services.Crawlers
+{
+    public class SitemapCrawler
+    {
+        private readonly UrlValidatorService _urlValidator;
+        private readonly RequestService _requestService;
+        private readonly SitemapParser _parserService;
+        private readonly LinkConvertorService _convertor;
+        public SitemapCrawler(
+            UrlValidatorService urlValidatorService,
+            RequestService requestService,
+            SitemapParser parserService,
+            LinkConvertorService convertorService)
+        {
+            _urlValidator = urlValidatorService;
+            _requestService = requestService;
+            _parserService = parserService;
+            _convertor = convertorService;
+        }
+       
+        public virtual IEnumerable<PerfomanceData> RunCrawler(string url)
+        {
+            var delay = 50;
+            var sitemapUrl = _convertor.GetDefaultSitemap(url);
+            var linkQueue = new Queue<string>();
+            var resultList = new List<PerfomanceData>();
+            linkQueue.Enqueue(sitemapUrl);
+
+            while (linkQueue.Count > 0)
+            {
+                var link = linkQueue.Dequeue();
+                var resultsContainsLink = resultList.Any(x => x.Url == link);
+                var notValidLink = !_urlValidator.IsValidLink(link);
+
+                if (notValidLink || resultsContainsLink)
+                {
+                    continue;
+                }
+
+                Thread.Sleep(delay);
+                var result = _requestService.ScanUrlAsync(link, out string xml);
+                var notContainsLinkSitemap = !link.Contains(".xml.gz");
+
+                if (notContainsLinkSitemap)
+                {
+                    resultList.Add(result);
+                }
+
+                var parsedLinks = _parserService.GetSitemapLinks(xml);
+
+                foreach (var newLink in parsedLinks)
+                {
+                    if (!linkQueue.Any(x => x == newLink))
+                    {
+                        linkQueue.Enqueue(newLink);
+                    }
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
