0e0b259d4c1fab230de6cecd29b98bd9f2d783ab
a.sobol <sobol.andrey.25@gmail.com>
2021-12-13 15:40:41 +0200

    added new service for url validation

---
+++ WebCrawler/Services/UrlValidatorService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Services
+{
+    public class UrlValidatorService
+    {
+        private readonly string[] _notValidWebExtension =
+            new[]
+            {
+                ".png",
+                ".jpeg",
+                ".jpg",
+                ".ico",
+                ".woff",
+                ".woff2",
+                ".js",
+                ".css",
+                ".json",
+                ".git",
+                ".ttf",                
+                "@",
+                "?",
+                "#"
+            };
+        
+        public virtual bool IsValidLink(string url)
+        {
+            foreach(var extension in _notValidWebExtension)
+            {
+                if (url.Contains(extension))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public virtual bool IsCorrectLink(string link)
+        {
+
+            var containsSheme = link.Contains(Uri.UriSchemeHttp) || link.Contains(Uri.UriSchemeHttps);
+            if (!containsSheme)
+            {
+                return false;
+            }
+
+            var isAbsoluteLink = Uri.TryCreate(link, UriKind.Absolute, out Uri uriResult);
+            if (!isAbsoluteLink)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual bool ContainsBaseUrl(string url, string baseUrl)
+        {
+            return url.Contains(baseUrl);
+        }
+    }
+}
