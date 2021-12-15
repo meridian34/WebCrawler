28efefa5b602ea963c6b1044a0926ce78688bed9
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 17:45:00 +0200

    UrlValidationServiceTests changed

--- WebCrawler.Tests/UrlValidationServiceTests.cs
+++ WebCrawler.Tests/UrlValidationServiceTests.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebCrawler.Services;
 using Xunit;
 
@@ -31,7 +27,6 @@ namespace WebCrawler.Tests
         public void ContainsBaseUrl_Null_ShouldThrowNullRefferenceException()
         {
             // arrange
-
             var service = new UrlValidatorService();
             var baseUrl = "https://www.ukad-group.com/";
             var expectedValue = "Object reference not set to an instance of an object.";
@@ -50,13 +45,13 @@ namespace WebCrawler.Tests
         [InlineData("https://www.facebook.com/ukadgroup/", true)]
         [InlineData("www.ukad-group.com/123/", false)]
         [InlineData("/123/", false)]
-        public void IsCorrectLink_Url_ShouldReturnBooleanValue(string url, bool expectedValue)
+        public void UrlIsValid_Url_ShouldReturnBooleanValue(string url, bool expectedValue)
         {
             // arrange
             var service = new UrlValidatorService();
 
             //act
-            var result = service.IsCorrectLink(url);
+            var result = service.UrlIsValid(url);
 
             //assert
             Assert.Equal(result, expectedValue);
@@ -68,34 +63,24 @@ namespace WebCrawler.Tests
         [InlineData("https://www.facebook.com/ukadgroup/", true)]
         [InlineData("www.ukad-group.com/123/", true)]
         [InlineData("www.ukad-group.com/123.png", false)]
-        [InlineData("www.ukad-group.com/123.jpeg", false)]
-        [InlineData("www.ukad-group.com/123.jpg", false)]
-        [InlineData("www.ukad-group.com/123.ico", false)]
-        [InlineData("www.ukad-group.com/123.woff", false)]
-        [InlineData("www.ukad-group.com/123.woff2", false)]
-        [InlineData("www.ukad-group.com/123.js", false)]
-        [InlineData("www.ukad-group.com/123.css", false)]
-        [InlineData("www.ukad-group.com/123.json", false)]
-        [InlineData("www.ukad-group.com/123.git", false)]
-        [InlineData("www.ukad-group.com/123.ttf", false)]
         [InlineData("www.ukad-group.com/123?p=1", false)]
         [InlineData("www.ukad-group.com/123#p123", false)]
         [InlineData("mailto:hi@ukad-group.com", false)]
         [InlineData("/123/", true)] 
-        public void IsValidLink_Url_ShouldReturnBooleanValue(string url, bool expectedValue)
+        public void LinkIsValid_Url_ShouldReturnBooleanValue(string url, bool expectedValue)
         {
             // arrange
             var service = new UrlValidatorService();
 
             //act
-            var result = service.IsValidLink(url);
+            var result = service.LinkIsValid(url);
 
             //assert
             Assert.Equal(result, expectedValue);
         }
 
         [Fact]
-        public void IsValidLink_Null_ShouldThrowNullRefferenceException()
+        public void LinkIsValid_Null_ShouldThrowNullRefferenceException()
         {
             // arrange
 
@@ -103,7 +88,7 @@ namespace WebCrawler.Tests
             var expectedValue = "Object reference not set to an instance of an object.";
 
             //act
-            var result = Assert.Throws<NullReferenceException>(() => service.IsValidLink(null));
+            var result = Assert.Throws<NullReferenceException>(() => service.LinkIsValid(null));
 
             //assert
             Assert.Equal(result.Message, expectedValue);
