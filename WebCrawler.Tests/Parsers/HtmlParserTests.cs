ab7b0fdc9bc2458ce82ded7f26777e33aad43436
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 17:40:58 +0200

    HtmlParserTests changed

--- WebCrawler.Tests/Parsers/HtmlParserTests.cs
+++ WebCrawler.Tests/Parsers/HtmlParserTests.cs
@@ -1,5 +1,6 @@
-using System;
+using Moq;
 using System.Linq;
+using WebCrawler.Services;
 using WebCrawler.Services.Parsers;
 using Xunit;
 
@@ -7,23 +8,57 @@ namespace WebCrawler.Tests
 {
     public class HtmlParserTests
     {
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(@" <a href=""/latest-projects/"" class=""navigation - new__link"" title=""PORTFOLIO""></a>", "/latest-projects/")]
-        [InlineData(@"<a href=""/latest-projects/""></a> <a href=""/company/about-us/"" ></a>", "/latest-projects/", "/company/about-us/")]
-        [InlineData(@"<link href=""/bundles/all.css?v=NmSNb6C7uOf2wrP1bmEYwyF9Ny5FWIRON_d9xDAtIw41"" rel=""preload"" as=""style"" media=""screen"">")]
-        [InlineData(@" <a href=""/latest-projects-param/?param1=24""></a>", "/latest-projects-param/?param1=24")]
-        [InlineData(@" <a href=""/latest-projects-point/#point1"" ></a>", "/latest-projects-point/#point1")]
-        [InlineData(@" <a href=""mailto:hi@ukad-group.com"" ></a>", "mailto:hi@ukad-group.com")]
-        public void GetHtmlLinks_HtmlData_ShouldReturnLinksList(string html, params string[] expectedValue)
+        private readonly Mock<UrlValidatorService> _urlValidatorService;
+        private readonly Mock<LinkConvertorService> _linkConvertorService;
+
+        public HtmlParserTests()
+        {
+            _urlValidatorService = new Mock<UrlValidatorService>();
+            _linkConvertorService = new Mock<LinkConvertorService>();
+        }
+
+        
+        [Fact]
+        public void GetHtmlLinks_HtmlDataWithRelativeLink_ShouldReturnLinksList()
         {
             // arrange
-            var service = new HtmlParser();
+            var html = @" <a href=""/latest-projects/"" class=""navigation - new__link"" title=""PORTFOLIO""></a>";
+            var expectedValue = new string[] { "https://www.ukad-group.com/latest-projects/" };
+            var root = "https://www.ukad-group.com/";
+            var link = "/latest-projects/";
+            var convertResult = "https://www.ukad-group.com/latest-projects/";
+
+            _urlValidatorService.SetupSequence(x => x.LinkIsValid(It.Is<string>(s => s == link))).Returns(true);
+            _urlValidatorService.SetupSequence(x => x.UrlIsValid(It.Is<string>(s => s == link))).Returns(false);
+            _urlValidatorService.SetupSequence(x => x.ContainsBaseUrl(It.Is<string>(s => s == link), It.Is<string>(s => s == root))).Returns(false);
+            _linkConvertorService.SetupSequence(x => x.ConvertRelativeToAbsolute(It.Is<string>(s => s == link), It.Is<string>(s => s == root))).Returns(convertResult);
+            var service = new HtmlParser(_urlValidatorService.Object, _linkConvertorService.Object);
 
             //act
-            var result = service.GetHtmlLinks(html).ToArray();
+            var result = service.GetHtmlLinks(html, root).ToArray();
             
+            //assert
+            Assert.Equal(result, expectedValue);
+        }
+
+        [Fact]
+        public void GetHtmlLinks_HtmlDataWithAbsoluteLink_ShouldReturnLinksList()
+        {
+            // arrange
+            var html = @" <a href=""https://www.ukad-group.com/latest-projects/"" class=""navigation - new__link"" title=""PORTFOLIO""></a>";
+            var expectedValue = new string[] { "https://www.ukad-group.com/latest-projects/" };
+            var sourceUrl = "https://www.ukad-group.com/123";
+            var root = "https://www.ukad-group.com/";
+            var link = "https://www.ukad-group.com/latest-projects/";
+            _linkConvertorService.SetupSequence(x => x.GetRootUrl(It.Is<string>(s => s == sourceUrl))).Returns(root);
+            _urlValidatorService.SetupSequence(x => x.LinkIsValid(It.Is<string>(s => s == link))).Returns(true);
+            _urlValidatorService.SetupSequence(x => x.UrlIsValid(It.Is<string>(s => s == link))).Returns(true);
+            _urlValidatorService.SetupSequence(x => x.ContainsBaseUrl(It.Is<string>(s => s == link), It.Is<string>(s => s == root))).Returns(true);
+            var service = new HtmlParser(_urlValidatorService.Object, _linkConvertorService.Object);
+
+            //act
+            var result = service.GetHtmlLinks(html, sourceUrl).ToArray();
+
             //assert
             Assert.Equal(result, expectedValue);
         }
