46ef64737f2cd95c70260a9acc3919619bd4842e
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 15:49:56 +0200

    SitemapCrawler logic changed

--- WebCrawler/Services/Crawlers/SitemapCrawler.cs
+++ WebCrawler/Services/Crawlers/SitemapCrawler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using WebCrawler.Models;
 using WebCrawler.Services.Parsers;
 
@@ -8,26 +7,22 @@ namespace WebCrawler.Services.Crawlers
 {
     public class SitemapCrawler
     {
-        private readonly UrlValidatorService _urlValidator;
         private readonly RequestService _requestService;
         private readonly SitemapParser _parserService;
         private readonly LinkConvertorService _convertor;
 
         public SitemapCrawler(
-            UrlValidatorService urlValidatorService,
             RequestService requestService,
             SitemapParser parserService,
             LinkConvertorService convertorService)
         {
-            _urlValidator = urlValidatorService;
             _requestService = requestService;
             _parserService = parserService;
             _convertor = convertorService;
         }
-       
+
         public virtual IEnumerable<Link> RunCrawler(string url)
         {
-            var delay = 50;
             var sitemapUrl = _convertor.GetDefaultSitemap(url);
             var linkQueue = new Queue<string>();
             var resultList = new List<Link>();
@@ -36,30 +31,21 @@ namespace WebCrawler.Services.Crawlers
             while (linkQueue.Count > 0)
             {
                 var link = linkQueue.Dequeue();
-                var resultsContainsLink = resultList.Any(x => x.Url == link);
-                var notValidLink = !_urlValidator.LinkIsValid(link);
-
-                if (notValidLink || resultsContainsLink)
-                {
-                    continue;
-                }
-
-                Thread.Sleep(delay);
                 var xml = _requestService.Download(link);
-                var notContainsLinkSitemap = !link.Contains(".xml");
-
-                if (notContainsLinkSitemap)
-                {
-                    resultList.Add(new Link() { Url = link, IsSitemap = true });
-                }
-
                 var parsedLinks = _parserService.GetSitemapLinks(xml);
 
                 foreach (var newLink in parsedLinks)
                 {
-                    if (!linkQueue.Any(x => x == newLink))
+                    var resultsContainsLink = resultList.Any(x => x.Url == newLink);
+                    var containsXmlExtension = newLink.Contains(".xml");
+                    
+                    if (containsXmlExtension)
                     {
                         linkQueue.Enqueue(newLink);
+                    }
+                    else if (!containsXmlExtension && !resultsContainsLink)
+                    {
+                        resultList.Add(new Link() { Url = newLink, IsSitemap = true });
                     }
                 }
             }
