f2f7d9873645cbcdeafddd87b6eafc13b7b3b963
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 17:42:15 +0200

    SitemapParserTests changed

--- WebCrawler.Tests/Parsers/SitemapParserTests.cs
+++ WebCrawler.Tests/Parsers/SitemapParserTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Moq;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WebCrawler.Services;
 using WebCrawler.Services.Parsers;
 using Xunit;
 
@@ -10,18 +8,37 @@ namespace WebCrawler.Tests
 {
     public class SitemapParserTests
     {
+        private readonly Mock<UrlValidatorService> _urlValidatorService = new Mock<UrlValidatorService>();
+
+        [Theory]
+        [InlineData(@"<loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/")]
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/</loc> <loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/latest-projects/", "https://www.ukad-group.com/")]
+        public void GetSitemapLinks_ValidSitemapXmlData_ShouldReturnLinksList(string html, params string[] expectedValue)
+        {
+            // arrange
+            _urlValidatorService.Setup(x => x.UrlIsValid(It.IsAny<string>())).Returns(true);
+
+            var service = new SitemapParser(_urlValidatorService.Object);
+
+            //act
+            var result = service.GetSitemapLinks(html).ToArray();
+
+            //assert
+            Assert.Equal(result, expectedValue);
+        }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
-        [InlineData(@"<loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/")]
-        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/</loc> <loc>https://www.ukad-group.com/</loc>", "https://www.ukad-group.com/latest-projects/", "https://www.ukad-group.com/")]
-        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/?param1=24</loc>", "https://www.ukad-group.com/latest-projects/?param1=24")]
-        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/#point1</loc>", "https://www.ukad-group.com/latest-projects/#point1")]
-        [InlineData(@"<loc>mailto:hi@ukad-group.com</loc>", "mailto:hi@ukad-group.com")]
-        public void GetSitemapLinks_SitemapXmlData_ShouldReturnLinksList(string html, params string[] expectedValue)
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/?param1=24</loc>")]
+        [InlineData(@"<loc>https://www.ukad-group.com/latest-projects/#point1</loc>")]
+        [InlineData(@"<loc>mailto:hi@ukad-group.com</loc>")]
+        public void GetSitemapLinks_NotValidSitemapXmlData_ShouldReturnEmptyLinksList(string html, params string[] expectedValue)
         {
             // arrange
-            var service = new SitemapParser();
+            _urlValidatorService.SetupSequence(x => x.UrlIsValid(It.IsAny<string>())).Returns(false);
+
+            var service = new SitemapParser(_urlValidatorService.Object);
 
             //act
             var result = service.GetSitemapLinks(html).ToArray();
