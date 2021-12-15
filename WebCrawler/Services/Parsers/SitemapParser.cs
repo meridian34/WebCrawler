8674e6f20f056e2de9548c67656494f5dbb8e635
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 15:48:50 +0200

    SitemapParser logic changed

--- WebCrawler/Services/Parsers/SitemapParser.cs
+++ WebCrawler/Services/Parsers/SitemapParser.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace WebCrawler.Services.Parsers
 {
     public class SitemapParser
     {
+        private readonly UrlValidatorService _urlValidatorService;
+
+        public SitemapParser(UrlValidatorService urlValidatorService)
+        {
+            _urlValidatorService = urlValidatorService;
+        }
+
         public virtual IEnumerable<string> GetSitemapLinks(string data)
         {
             var linkList = new List<string>();
@@ -30,7 +38,12 @@ namespace WebCrawler.Services.Parsers
                     break;
                 }
 
-                linkList.Add(data.Substring(startIndexTag + startTag.Length, endIndexTag - (startIndexTag + startTag.Length)));
+                var link = data.Substring(startIndexTag + startTag.Length, endIndexTag - (startIndexTag + startTag.Length));
+                if (_urlValidatorService.UrlIsValid(link))
+                {
+                    linkList.Add(link);
+                }
+                
                 position = endIndexTag + endTag.Length;
             }
 
