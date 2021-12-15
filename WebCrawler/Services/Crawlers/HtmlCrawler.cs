94c0c16b60373274801322c39179dce7470afce2
a.sobol <sobol.andrey.25@gmail.com>
2021-12-15 15:44:40 +0200

    HtmlCrawler logic have been changed

--- WebCrawler/Services/Crawlers/HtmlCrawler.cs
+++ WebCrawler/Services/Crawlers/HtmlCrawler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using WebCrawler.Models;
 using WebCrawler.Services.Parsers;
 
@@ -8,18 +7,15 @@ namespace WebCrawler.Services.Crawlers
 {
     public class HtmlCrawler
     {
-        private readonly UrlValidatorService _validator;
         private readonly RequestService _requestService;
         private readonly HtmlParser _parserService;
         private readonly LinkConvertorService _convertor;
 
         public HtmlCrawler(
-            UrlValidatorService urlValidatorService,
             RequestService requestService,
             HtmlParser parserService,
             LinkConvertorService linkConvertorService)
         {
-            _validator = urlValidatorService;
             _requestService = requestService;
             _parserService = parserService;
             _convertor = linkConvertorService;
@@ -27,42 +23,31 @@ namespace WebCrawler.Services.Crawlers
 
         public virtual IEnumerable<Link> RunCrawler(string url)
         {
-            var delay = 50;
             var baseUrl = _convertor.GetRootUrl(url);
             var linkQueue = new Queue<string>();
             var resultList = new List<Link>();
+            resultList.Add(new Link { IsCrawler = true, Url = baseUrl });
             linkQueue.Enqueue(baseUrl);
 
             while (linkQueue.Count > 0)
             {
                 var link = linkQueue.Dequeue();
-                var resultsContainsLink = resultList.Any(x => x.Url == link);
-                var notValidLink = !_validator.LinkIsValid(link);
-                var linkIsNotCorrect = !_validator.LinkIsCorrect(link)
-                    || !_validator.ContainsBaseUrl(link, baseUrl);
-
-                if (notValidLink || resultsContainsLink || linkIsNotCorrect)
-                {
-                    continue;
-                }
-
-                Thread.Sleep(delay);
-                resultList.Add(new Link() { Url = link, IsCrawler = true });
                 var html = _requestService.Download(link);
-                var parsedLinks = _parserService.GetHtmlLinks(html);
-
+                var parsedLinks = _parserService.GetHtmlLinks(html, link);
+                
                 foreach (var newLink in parsedLinks)
                 {
-                    var correctLink = _validator.ContainsBaseUrl(newLink, baseUrl)
-                         ? newLink
-                         : _convertor.ConvertRelativeToAbsolute(newLink, baseUrl);
+                    var queueContainsUrl = linkQueue.Any(x => x == newLink);
+                    var resultsContainsUrl = resultList.Any(x => x.Url == newLink);
 
-                    var notContainsLink = !linkQueue.Any(x => x == correctLink);
-
-                    if (notContainsLink)
+                    if (!resultsContainsUrl)
                     {
-                        
-                        linkQueue.Enqueue(correctLink);
+                        resultList.Add(new Link { IsCrawler = true, Url = newLink });
+                    }
+
+                    if (!resultsContainsUrl && !queueContainsUrl)
+                    {
+                        linkQueue.Enqueue(newLink);
                     }
                 }
             }
