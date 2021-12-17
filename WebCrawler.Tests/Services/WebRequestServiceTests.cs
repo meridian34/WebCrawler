49f9ff5bdca68d5d3bbee5823f99f625e9fc2f5a
a.sobol <sobol.andrey.25@gmail.com>
2021-12-17 16:14:16 +0200

    Added tests for WebRequestService

---
+++ WebCrawler.Tests/Services/WebRequestServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+using WebCrawler.Services;
+using Xunit;
+
+namespace WebCrawler.Tests.Services
+{
+    public class WebRequestServiceTests
+    {
+        [Fact]
+        public async Task DownloadAsync_Url_ShouldReturnStringDataAsync()
+        {
+            // arrange
+            var url = new Uri("https://www.ukad-group.com/");
+            var expectedValue = "xml or html data";
+            var httpClient = new Mock<HttpClientService>();
+            var response = new HttpResponseMessage();
+            response.Content = new StringContent(expectedValue);
+            httpClient.Setup(x => x.GetAsync(It.IsAny<Uri>())).ReturnsAsync(response);
+
+            var webRequestService = new WebRequestService(httpClient.Object);
+
+            //act
+            var data = await webRequestService.DownloadAsync(url);
+
+            //assert
+            Assert.Equal(expectedValue, data);
+        }
+
+        [Fact]
+        public async Task DownloadAsync2_Url_ShouldReturnStringDataAsync()
+        {
+            // arrange
+            var url = new Uri("https://www.ukad-group.com/");
+            var inputValue = new Link[] { new Link { Url = url } };
+
+            var expectedValue = new PerfomanceData[] { new PerfomanceData { ElapsedMilliseconds = 50, Url = url } };
+            var httpClient = new Mock<HttpClientService>();
+            var response = new HttpResponseMessage();
+            httpClient.Setup(x => x.GetAsync(It.IsAny<Uri>())).ReturnsAsync(
+                () => 
+                {
+                    Task.Delay(50).GetAwaiter().GetResult();
+                    return response;
+                });
+
+            var webRequestService = new WebRequestService(httpClient.Object);
+
+            //act
+            var data = await webRequestService.GetElapsedTimeForLinksAsync(inputValue);
+
+            //assert
+            Assert.InRange<int>(data.First().ElapsedMilliseconds, 50, 500);
+        }
+    }
+}
