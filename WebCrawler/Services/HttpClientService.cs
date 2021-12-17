cfaacd472b674a405c0aea984d312f76581bebe7
a.sobol <sobol.andrey.25@gmail.com>
2021-12-17 15:45:25 +0200

    a wrapper class was added (HttpClient inside)

---
+++ WebCrawler/Services/HttpClientService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Services
+{
+    public class HttpClientService
+    {
+        public virtual async Task<HttpResponseMessage> GetAsync(Uri url)
+        {
+            var client = new HttpClient();
+            return await client.GetAsync(url);
+        }
+    }
+}
