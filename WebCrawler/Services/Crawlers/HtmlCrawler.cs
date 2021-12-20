b26d409eafadb956667d2f3659d8607882077b21
a.sobol <sobol.andrey.25@gmail.com>
2021-12-20 15:30:26 +0200

    logic html crawler have been modified. new model (CrawlerData) introduced

--- WebCrawler/Services/Crawlers/HtmlCrawler.cs
+++ WebCrawler/Services/Crawlers/HtmlCrawler.cs
@@ -13,10 +13,7 @@ namespace WebCrawler.Services.Crawlers
         private readonly HtmlParser _parserService;
         private readonly LinkConvertorService _convertor;
 
-        public HtmlCrawler(
-            WebRequestService requestService,
-            HtmlParser parserService,
-            LinkConvertorService linkConvertorService)
+        public HtmlCrawler(WebRequestService requestService, HtmlParser parserService, LinkConvertorService linkConvertorService)
         {
             _requestService = requestService;
             _parserService = parserService;
@@ -26,32 +23,33 @@ namespace WebCrawler.Services.Crawlers
         public virtual async Task<IEnumerable<Link>> RunCrawlerAsync(Uri url)
         {
             var baseUrl = _convertor.GetRootUrl(url);
-            var resultList = new List<Link>();
-            resultList.Add(new Link { Url = baseUrl });            
-
-            while (resultList.Any(x=> !x.IsCrawler ))
+            var resultList = new List<CrawlerData> { new CrawlerData{ Url = baseUrl } };
+            
+            while (resultList.Any(x=> !x.Scanned ))
             {
-                var link = resultList.First(x => !x.IsCrawler);
+                var link = resultList.First(x => !x.Scanned);
                 var html = await _requestService.DownloadAsync(link.Url);
-                link.IsCrawler = true;
+                link.Scanned = true;
                 var parsedLinks = _parserService.GetHtmlLinks(html, link.Url);
-
-                FilterLinksAndAdd(parsedLinks, resultList);
+                
+                resultList.AddRange(GetUniqueLinks(parsedLinks, resultList));
             }
 
-            return resultList;
+            return resultList.Select(x=> new Link { Url = x.Url, FromHtml = true });
         }
 
-        private void FilterLinksAndAdd(IEnumerable<Uri> filterLinks, List<Link> resultList)
+        private IEnumerable<CrawlerData> GetUniqueLinks(IEnumerable<Uri> filterLinks, IEnumerable<CrawlerData> targetList)
         {
+            var resultList = new Queue<CrawlerData>();
             foreach (var link in filterLinks)
             {
-                var resultsContainsUrl = resultList.Any(x => x.Url == link);
+                var resultsContainsUrl = targetList.Any(x => x.Url == link);
                 if (!resultsContainsUrl)
                 {
-                    resultList.Add(new Link { Url = link });
+                    resultList.Enqueue(new CrawlerData { Url = link });
                 }
             }
+            return resultList;
         }
     }
 }
