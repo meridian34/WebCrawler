0e6af62d97a8c297b12c72e3d66882be6f490438
a.sobol <sobol.andrey.25@gmail.com>
2021-12-20 17:18:01 +0200

    Added Sturtup class with DI-container

---
+++ WebCrawler.Service/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using WebCrawler.ConsoleApplication.Services;
+
+namespace WebCrawler.ConsoleApplication
+{
+    public class Startup
+    {
+        private static readonly IServiceProvider _provider;
+        static Startup()
+        {
+            var service = new ServiceCollection();
+            service.AddWebCrawler();
+            service.AddTransient<ConsoleService>();
+            service.AddTransient<CrawlerService>();
+            _provider = service.BuildServiceProvider();
+        }
+
+        public static CrawlerService GetWebCrawler => _provider.GetService<CrawlerService>();
+    }
+}
