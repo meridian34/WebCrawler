78dd0da60dabf3d2c6be45c0f8b1fda8291e6761
a.sobol <sobol.andrey.25@gmail.com>
2021-12-20 17:14:06 +0200

    Added crawler extension for DI (DIExtension)

---
+++ WebCrawler/DIExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebCrawler.Services;
+using WebCrawler.Services.Crawlers;
+using WebCrawler.Services.Parsers;
+
+namespace WebCrawler
+{
+    public static class DIExtension
+    {
+        public static IServiceCollection AddWebCrawler(this IServiceCollection services)
+        {   
+            services.AddTransient<UrlValidatorService>();
+            services.AddTransient<LinkConvertorService>();
+            services.AddTransient<SitemapParser>();
+            services.AddTransient<HtmlParser>();
+            services.AddTransient<HttpClientService>();
+            services.AddTransient<WebRequestService>();
+            services.AddTransient<SitemapCrawler>();
+            services.AddTransient<HtmlCrawler>();
+            services.AddTransient<WebCrawlerService>();
+
+            return services;
+        }
+    }
+}
