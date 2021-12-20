ecd3a88dd6d5eb31b65c9398a2993770e9b85d14
a.sobol <sobol.andrey.25@gmail.com>
2021-12-20 17:22:28 +0200

    Removed initialization of many classes. Added DI call and application launch.

--- WebCrawler.Service/Program.cs
+++ WebCrawler.Service/Program.cs
@@ -1,8 +1,4 @@
-﻿using WebCrawler.Services;
-using WebCrawler.Services.Crawlers;
-using WebCrawler.Services.Parsers;
-using WebCrawler.ConsoleApplication.Services;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace WebCrawler.ConsoleApplication
 {
@@ -10,12 +6,7 @@ namespace WebCrawler.ConsoleApplication
     {
         public static async Task Main(string[] args)
         {
-            var htmlCrawler = new HtmlCrawler( new WebRequestService( new HttpClientService()), new HtmlParser(new UrlValidatorService(),new LinkConvertorService()), new LinkConvertorService());
-            var sitemapCrawler = new SitemapCrawler( new WebRequestService( new HttpClientService()), new SitemapParser(new UrlValidatorService()), new LinkConvertorService());
-            var webCrawler = new WebCrawlerService(htmlCrawler, sitemapCrawler, new UrlValidatorService(), new WebRequestService(new HttpClientService()));
-            var consoleService = new ConsoleService();
-            var crawlerService = new CrawlerService(consoleService, webCrawler);
-            await crawlerService.RunAsync();
+            await Startup.GetWebCrawler.RunAsync();
         }
     }
 }
