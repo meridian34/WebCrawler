735c5fca5140c41d1b8bb3a96ff8e73f1a65d335
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 10:57:01 +0200

    add Link entity

---
+++ WebCrawler.EntityFramework/Entities/Link.cs
@@ -0,0 +1,18 @@
+﻿namespace WebCrawler.EntityFramework.Entities
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool FromSitemap { get; set; }
+
+        public bool FromHtml { get; set; }
+
+        public int? ElapsedMilliseconds { get; set; }
+
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+    }
+}
