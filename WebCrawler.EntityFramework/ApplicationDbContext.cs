dc1114e82660afb97126729fe660f05056623b0a
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 10:58:36 +0200

    Added application db context

---
+++ WebCrawler.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Data;
+using WebCrawler.EntityFramework.Entities;
+using WebCrawler.EntityFramework.EntityConfigurations;
+
+namespace WebCrawler.EntityFramework
+{
+    public class ApplicationDbContext : DbContext, IEfRepositoryDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Link> Links { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new LinkConfiguration());
+            modelBuilder.ApplyConfiguration(new TestConfiguration());
+        }
+    }
+}
