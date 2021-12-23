9c73881a947985ec89092925d0ae2e463957d462
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 14:15:08 +0200

    added indentation and hyphenation

--- WebCrawler.EntityFramework/EntityConfigurations/LinkConfiguration.cs
+++ WebCrawler.EntityFramework/EntityConfigurations/LinkConfiguration.cs
@@ -9,11 +9,22 @@ namespace WebCrawler.EntityFramework.EntityConfigurations
         public void Configure(EntityTypeBuilder<Link> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            builder.Property(x => x.Url).HasMaxLength(2000).IsRequired();
-            builder.Property(x => x.FromSitemap).IsRequired();
-            builder.Property(x => x.FromHtml).IsRequired();
-            builder.Property(x => x.ElapsedMilliseconds).IsRequired(false);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Url)
+                .HasMaxLength(2000)
+                .IsRequired();
+
+            builder.Property(x => x.FromSitemap)
+                .IsRequired();
+
+            builder.Property(x => x.FromHtml)
+                .IsRequired();
+
+            builder.Property(x => x.ElapsedMilliseconds)
+                .IsRequired(false);
 
             builder.HasOne(x => x.Test)
                 .WithMany(z => z.Links)
--- WebCrawler.EntityFramework/EntityConfigurations/TestConfiguration.cs
+++ WebCrawler.EntityFramework/EntityConfigurations/TestConfiguration.cs
@@ -9,9 +9,16 @@ namespace WebCrawler.EntityFramework.EntityConfigurations
         public void Configure(EntityTypeBuilder<Test> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            builder.Property(x => x.UserLink).HasMaxLength(2000).IsRequired();
-            builder.Property(x => x.TestDateTime).IsRequired();
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.UserLink)
+                .HasMaxLength(2000)
+                .IsRequired();
+
+            builder.Property(x => x.TestDateTime)
+                .IsRequired();
         }
     }
 }
