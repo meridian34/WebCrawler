3c2c77b5b33b149a2bd000d56e3c3fd7f6916b99
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 10:57:54 +0200

    Added configurations for entities

---
+++ WebCrawler.EntityFramework/EntityConfigurations/LinkConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebCrawler.EntityFramework.Entities;
+
+namespace WebCrawler.EntityFramework.EntityConfigurations
+{
+    public class LinkConfiguration : IEntityTypeConfiguration<Link>
+    {
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Url).HasMaxLength(2000).IsRequired();
+            builder.Property(x => x.FromSitemap).IsRequired();
+            builder.Property(x => x.FromHtml).IsRequired();
+            builder.Property(x => x.ElapsedMilliseconds).IsRequired(false);
+
+            builder.HasOne(x => x.Test)
+                .WithMany(z => z.Links)
+                .HasForeignKey(x => x.TestId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ WebCrawler.EntityFramework/EntityConfigurations/TestConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebCrawler.EntityFramework.Entities;
+
+namespace WebCrawler.EntityFramework.EntityConfigurations
+{
+    public class TestConfiguration : IEntityTypeConfiguration<Test>
+    {
+        public void Configure(EntityTypeBuilder<Test> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.UserLink).HasMaxLength(2000).IsRequired();
+            builder.Property(x => x.TestDateTime).IsRequired();
+        }
+    }
+}
