352f332a5bf051c178b3779ed1a2ee2e0c555a6b
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 11:03:43 +0200

    Added extension for DI

---
+++ WebCrawler.EntityFramework/Extensions/DIExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace WebCrawler.EntityFramework.Extensions
+{
+    public static class DIExtension
+    {
+        public static IServiceCollection AddWebCrawlerRepository(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddEfRepository<ApplicationDbContext>(options);
+            services.AddScoped<DataProvider>();
+         
+            return services;
+        }
+    }
+}
