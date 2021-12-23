69fd7e1f6368f42cb8f3061a9e1ddeca04568f5b
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 16:24:55 +0200

    Added DI extension

---
+++ WebCrawler.Logic/Extensions/DIExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebCrawler.Logic.Providers;
+using WebCrawler.Logic.Services;
+
+namespace WebCrawler.Logic.Extensions
+{
+    public static class DIExtension
+    {
+        public static IServiceCollection AddWebCrawlerLogic(this IServiceCollection services)
+        {
+            services.AddScoped<DataProvider>();
+            services.AddScoped<DataStorageService>();
+
+            return services;
+        }
+    }
+}
