93208529a431b5a5a8f1a135a734b8e0810c8065
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 10:57:18 +0200

    added Test entity

---
+++ WebCrawler.EntityFramework/Entities/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebCrawler.EntityFramework.Entities
+{
+    public class Test
+    {
+        public int Id { get; set; }
+
+        public string UserLink { get; set; }
+
+        public DateTimeOffset TestDateTime { get; set; }
+
+        public List<Link> Links { get; set; }
+    }
+}
