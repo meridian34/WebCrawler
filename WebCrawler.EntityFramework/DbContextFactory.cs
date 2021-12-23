a53d3ae716e2cff4d13a9b916a4256c796274550
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 11:01:28 +0200

    added a context factory that implements IDesignTimeDbContextFactory. That it would be possible to create migrations from the Package Manager Console

---
+++ WebCrawler.EntityFramework/DbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebCrawler.EntityFramework
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-5NI0SMB; Initial Catalog=WebCrawlerDB; Integrated Security=True");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
