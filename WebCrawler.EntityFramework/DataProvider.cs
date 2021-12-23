c6cf1c70b492d84a771c2c9fb3b808a0d3a8c236
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 14:23:09 +0200

    deleted unused methods

--- WebCrawler.EntityFramework/DataProvider.cs
+++ WebCrawler.EntityFramework/DataProvider.cs
@@ -29,31 +29,5 @@ namespace WebCrawler.EntityFramework
             await _testRepository.SaveChangesAsync();
         }
 
-        public virtual IEnumerable<Test> GetTestsByPage(int page, int countItems)
-        {
-            return _testRepository
-                .GetAll()         
-                .Skip((page - 1) * countItems)
-                .Take(countItems)
-                .OrderByDescending(x => x.TestDateTime);
-        }
-
-        public virtual IEnumerable<Link> GetLinksByPage(int testId, int page, int countItems)
-        {
-            return _linkRepository
-                .GetAll()
-                .Where(x=>x.TestId == testId)
-                .Skip((page - 1) * countItems)
-                .Take(countItems)
-                .OrderBy(x => x.ElapsedMilliseconds);
-        }
-
-        public virtual IEnumerable<Test> GetTestsByUrl(string url)
-        {
-            return _testRepository
-                .Include(x => x.Links)
-                .Where(x => x.UserLink.Contains(url))
-                .OrderByDescending(x => x.TestDateTime);
-        }
     }
 }
