8afa51a46e104062822cacb7136fcb11e7557083
a.sobol <sobol.andrey.25@gmail.com>
2021-12-23 16:21:20 +0200

    Added DataProvider class for working with Ukad.Irepository

---
+++ WebCrawler.Logic/Providers/DataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.EntityFramework.Entities;
+
+namespace WebCrawler.Logic.Providers
+{
+    public class DataProvider
+    {
+        private readonly IRepository<Test> _testRepository;
+        
+        public DataProvider(IRepository<Test> testRepository)
+        {
+            _testRepository = testRepository;
+        }
+
+        public virtual async Task SaveTestResultAsync(string userUrl, IEnumerable<Link> links)
+        {
+
+            await _testRepository.AddAsync(new Test
+            {
+                UserLink = userUrl,
+                TestDateTime = DateTimeOffset.Now,
+                Links = links.ToList()
+            });
+            await _testRepository.SaveChangesAsync();
+        }
+    }
+}
