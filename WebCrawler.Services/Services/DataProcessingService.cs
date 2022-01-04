c3551af4ca158b761dc56800ab01bb6533abe791
Andrey <sobol.andrey.25@gmail.com>
2022-01-04 16:26:52 +0200

    added a new service for working with WebCrawlerService and repositories

---
+++ WebCrawler.Services/Services/DataProcessingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebCrawler.Services.Models;
+
+namespace WebCrawler.Services.Services
+{
+    public class DataProcessingService
+    {
+        private readonly DataStorageService _storage;
+        private readonly WebCrawlerService _webCrawler;
+
+        public DataProcessingService(DataStorageService storage, WebCrawlerService webCrawler)
+        {
+            _storage = storage;
+            _webCrawler = webCrawler;
+        }
+
+        public virtual async Task StartCrawlingSite(Uri url)
+        {
+            var links = await _webCrawler.GetLinksAsync(url);
+            var perfomanceData = await _webCrawler.GetPerfomanceDataCollectionAsync(links);
+            await _storage.SaveAsync(url.OriginalString, links, perfomanceData);
+        } 
+
+        public virtual async Task<TestsPage> GetTestsPage(int pageNumber, int pageSize)
+        {
+            return await _storage.GetTestsByPageAsync(pageNumber, pageSize);
+        }
+
+        public virtual async Task GetLinksPage()
+        {
+
+        }
+    }
+}
