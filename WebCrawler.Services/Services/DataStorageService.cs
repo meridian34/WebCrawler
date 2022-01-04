078693c77cc1ec0fdc207763555ff07109cdf153
Andrey <sobol.andrey.25@gmail.com>
2022-01-04 16:14:20 +0200

    Added logic for work with CrawlerService, repositories and changed namespace

--- WebCrawler.Services/Services/DataStorageService.cs
+++ WebCrawler.Services/Services/DataStorageService.cs
@@ -4,42 +4,95 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using WebCrawler.EntityFramework.Entities;
+using WebCrawler.Services.Models;
 
-namespace WebCrawler.Logic.Services
+namespace WebCrawler.Services.Services
 {
     public class DataStorageService
     {
-        private readonly IRepository<Test> _testRepository;
+        private readonly IRepository<TestEntity> _testRepository;
+        private readonly IRepository<LinkEntity> _linkRepository;
 
-        public DataStorageService(IRepository<Test> testRepository)
+        public DataStorageService(IRepository<TestEntity> testRepository, IRepository<LinkEntity> linkRepository)
         {
             _testRepository = testRepository;
+            _linkRepository = linkRepository;
         }
 
-        public virtual async Task SaveAsync(string userUrl, IEnumerable<Models.Link> links, IEnumerable<Models.PerfomanceData> perfomances)
+        /// <returns>returns the Id of the created record (Entity.Test)</returns>
+        public virtual async Task<int> SaveAsync(string userUrl, IEnumerable<WebCrawler.Models.Link> links, IEnumerable<WebCrawler.Models.PerfomanceData> perfomances)
         {
             var colectionForSave = CreateSavedCollection(links, perfomances);
-            await SaveTestResultAsync(userUrl, colectionForSave);
+            return await SaveTestResultAsync(userUrl, colectionForSave);
         }
 
-        private async Task SaveTestResultAsync(string userUrl, IEnumerable<Link> links)
+        public virtual async Task<TestsPage> GetTestsByPageAsync(int pageNumber, int pageSize)
         {
-            await _testRepository.AddAsync(new Test
+            var page = new TestsPage() { CurrentPage = pageNumber, ItemsCount = pageSize, Tests = new List<Test>() };
+            var query = _testRepository.GetAll().OrderByDescending(x => x.TestDateTime);
+            var (TotalCount, Result) = await _testRepository.GetPageAsync(query, pageNumber, pageSize);
+            page.TotalPages = (int)Math.Ceiling( TotalCount / (decimal)pageSize);
+            page.Tests = Result.Select(x => new Test
+            {
+                Id = x.Id,
+                TestDateTime = x.TestDateTime,
+                UserLink = x.UserLink
+            });
+
+            return page;
+        }
+
+        public virtual async Task<LinksPage> GetLinksPageByTestIdAsync(int testId)
+        {
+            var page = new LinksPage
+            {
+                Url = (await _testRepository.GetByIdAsync(testId)).UserLink
+            };
+
+            var links = _linkRepository
+               .GetAllAsNoTracking()
+               .Where(x => x.TestId == testId)
+               .OrderBy(x => x.ElapsedMilliseconds);
+
+            page.Links = links
+               .Select(x => new Link
+               {
+                   ElapsedMilliseconds = x.ElapsedMilliseconds,
+                   Id = x.Id,
+                   Url = x.Url
+               });
+
+            page.HtmlLinks = links
+                .Where(x=>x.FromHtml && !x.FromSitemap)
+                .Select(x => x.Url);
+
+            page.SitemapLinks = links
+                .Where(x => !x.FromHtml && x.FromSitemap)
+                .Select(x => x.Url);
+
+            return page;
+        }
+
+        private async Task<int> SaveTestResultAsync(string userUrl, IEnumerable<LinkEntity> links)
+        {
+            var test = new TestEntity
             {
                 UserLink = userUrl,
                 TestDateTime = DateTimeOffset.Now,
                 Links = links.ToList()
-            });
+            };
+            await _testRepository.AddAsync(test);
             await _testRepository.SaveChangesAsync();
+            return test.Id;
         }
 
-        private IEnumerable<Link> CreateSavedCollection(IEnumerable<Models.Link> links, IEnumerable<Models.PerfomanceData> perfomances)
+        private IEnumerable<LinkEntity> CreateSavedCollection(IEnumerable<WebCrawler.Models.Link> links, IEnumerable<WebCrawler.Models.PerfomanceData> perfomances)
         {
-            var listForSave = new List<Link>();
+            var listForSave = new List<LinkEntity>();
 
             foreach (var item in links)
             {
-                var link = new Link();
+                var link = new LinkEntity();
                 link.Url = item.Url.AbsoluteUri;
                 link.FromHtml = item.FromHtml;
                 link.FromSitemap = item.FromSitemap;
