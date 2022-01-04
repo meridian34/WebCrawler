0e580a0905401f3627ac0f2e28605b27eb7100a4
Andrey <sobol.andrey.25@gmail.com>
2022-01-04 16:24:39 +0200

    added models for page application

---
+++ WebCrawler.Services/Models/LinksPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebCrawler.Services.Models
+{
+    public class LinksPage
+    {
+        public string Url { get; set; }
+        public IEnumerable<Link> Links { get; set; }
+        public IEnumerable<string> SitemapLinks { get; set; }
+        public IEnumerable<string> HtmlLinks { get; set; }
+    }
+}
---
+++ WebCrawler.Services/Models/TestsPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebCrawler.Services.Models
+{
+    public class TestsPage
+    {
+        public int ItemsCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<Test> Tests { get; set; }
+    }
+}
