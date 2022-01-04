f6901f35c354bd0ee5da8043224a1083d4f2ead4
Andrey <sobol.andrey.25@gmail.com>
2022-01-04 16:24:03 +0200

    Added models

---
+++ WebCrawler.Services/Models/Link.cs
@@ -0,0 +1,15 @@
+﻿namespace WebCrawler.Services.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool FromSitemap { get; set; }
+
+        public bool FromHtml { get; set; }
+
+        public int? ElapsedMilliseconds { get; set; }
+    }
+}
---
+++ WebCrawler.Services/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebCrawler.Services.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+
+        public string UserLink { get; set; }
+
+        public DateTimeOffset TestDateTime { get; set; }
+    }
+}
