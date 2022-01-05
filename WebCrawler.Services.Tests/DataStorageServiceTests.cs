46853b9d3ef9f7e94898366b20675d181f55ef5d
Andrey <sobol.andrey.25@gmail.com>
2022-01-05 14:30:01 +0200

    fixed tests and added new tests

--- WebCrawler.Services.Tests/DataStorageServiceTests.cs
+++ WebCrawler.Services.Tests/DataStorageServiceTests.cs
@@ -7,8 +7,10 @@ using Xunit;
 using WebCrawler.Models;
 using System.Threading;
 using WebCrawler.Services.Services;
+using WebCrawler.EntityFramework.Entities;
+using System.Linq;
 
-namespace WebCrawler.Logic.Tests
+namespace WebCrawler.Services.Tests
 {
     public class DataStorageServiceTests
     {
@@ -16,16 +18,16 @@ namespace WebCrawler.Logic.Tests
         public async Task SaveAsync_WebCrawlerResults_ShouldConvrtrAndTransferToRepository()
         {
             // arrange
-            var testRepository = new Mock<IRepository<EntityFramework.Entities.TestEntity>>();
-            var linkRepository = new Mock<IRepository<EntityFramework.Entities.LinkEntity>>();
+            var testRepository = new Mock<IRepository<TestEntity>>();
+            var linkRepository = new Mock<IRepository<LinkEntity>>();
             var storage = new DataStorageService(testRepository.Object, linkRepository.Object);
             var inputUrl = "https://www.example.com/";
             var inputLinkCollection = new Link[] { new Link { Url = new Uri(inputUrl), FromHtml = true, FromSitemap = true } };
             var inputPerfomanceCollection = new PerfomanceData[] { new PerfomanceData { Url = new Uri(inputUrl), ElapsedMilliseconds = 200 } };
             var testDateTime = DateTimeOffset.Now;
-            var expectedLinkList = new List<EntityFramework.Entities.LinkEntity>()
+            var expectedLinkList = new List<LinkEntity>()
             {
-                new EntityFramework.Entities.LinkEntity
+                new LinkEntity
                 {
                     Url = inputUrl,
                     FromHtml = true,
@@ -33,7 +35,7 @@ namespace WebCrawler.Logic.Tests
                     ElapsedMilliseconds = 200
                 }
             };
-            var expectedResult = new EntityFramework.Entities.TestEntity { TestDateTime = testDateTime, UserLink = inputUrl, Links = expectedLinkList };
+            var expectedResult = new TestEntity { TestDateTime = testDateTime, UserLink = inputUrl, Links = expectedLinkList };
 
             //act
             await storage.SaveAsync(inputUrl, inputLinkCollection, inputPerfomanceCollection);
@@ -43,6 +45,34 @@ namespace WebCrawler.Logic.Tests
                 t.UserLink == inputUrl
                 && t.Links.Count == expectedLinkList.Count), It.IsAny<CancellationToken>()));
             testRepository.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()));
+        }
+
+        [Fact(Timeout = 1000)]
+        public async Task GetLinksPageByTestIdAsync_TestId_ShouldReturnLinksPage()
+        {
+            // arrange
+            var testRepository = new Mock<IRepository<TestEntity>>();
+            var linkRepository = new Mock<IRepository<LinkEntity>>();
+            var inputId = 100;
+            var expectedResultCount = 1;
+            var expectedLink = "www.example.com";
+
+            var item1 = new TestEntity { UserLink = expectedLink };
+            var listItems = new List<LinkEntity> { new LinkEntity { TestId = inputId, ElapsedMilliseconds = 50 } };
+
+            testRepository.SetupSequence(x => x.GetByIdAsync(It.Is<int>(x => x == inputId), default)).ReturnsAsync(item1);
+
+            linkRepository.Setup(x => x.GetAllAsNoTracking()).Returns(listItems.AsQueryable());
+            
+
+            var storage = new DataStorageService(testRepository.Object, linkRepository.Object);
+
+            // act 
+            var result = await storage.GetLinksPageByTestIdAsync(100);
+
+            //assert
+            Assert.Equal(expectedLink, result.Url);
+            Assert.Equal(expectedResultCount, result.Links.Count());
         }
     }
 }
